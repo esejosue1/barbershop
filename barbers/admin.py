19e0a118ad43f6e72bfe02d37353442e2f4970f9
esejosue <josuegallardolara@hotmail.com>
2023-03-22 19:02:53 -0700

    Finished the model for barbers, moving to the making of appointmnets feature

--- barbers/admin.py
+++ barbers/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Barbers
 
 # Register your models here.
+class BarbersAdmin(admin.ModelAdmin):
+    list_display=('first_name', 'last_name', 'email')
+
+admin.site.register(Barbers,BarbersAdmin)
--- barbers/models.py
+++ barbers/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Barbers(models.Model):
+    first_name=models.CharField(max_length=20)
+    last_name=models.CharField(max_length=20)
+    image=models.ImageField(upload_to='images/barbers')
+    title=models.CharField(max_length=100, null=True)
+    phone_number=models.CharField(max_length=15)
+    email=models.EmailField(max_length=100)
+    instragram=models.CharField(max_length=500, blank=True)
+    facebook=models.CharField(max_length=500, blank=True)
+    twitter=models.CharField(max_length=500, blank=True)
+
+    class Meta:
+        verbose_name='Barber'
+        verbose_name_plural='Barbers'
+
+    def __str__(self):
+        return self.first_name
+  
