9eb8c77a2556fc121e38f4cda1014820a8da20d9
esejosue <josuegallardolara@hotmail.com>
2023-03-22 19:57:29 -0700

    Created the appointments model with a foreign key of the barbers, thinking on how to connect the appointment model to the service model.

--- appointments/admin.py
+++ appointments/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import BusinessHour
+from .models import BusinessHour, Appointments
 
 # Register your models here.
 
@@ -7,4 +7,8 @@ class businessHoursAdmin(admin.ModelAdmin):
     list_display = ('day', 'opening_time', 'closing_time')
     list_filter = ('day',)
 
+class AppointmentsAdmin(admin.ModelAdmin):
+    list_display=('day','time','service', 'barber')
+
 admin.site.register(BusinessHour, businessHoursAdmin)
+admin.site.register(Appointments,AppointmentsAdmin)
--- appointments/models.py
+++ appointments/models.py
@@ -1,8 +1,22 @@
-from django.db import models
-from django.utils.translation import gettext_lazy as _
-from django.db import models
-from django.utils.translation import gettext_lazy as _
 
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from category.models import Services
+from barbers.models import Barbers
+from datetime import datetime
+
+TIME_CHOICES = (
+    ("10 AM", "10 AM"),
+    ("11 AM", "11 AM"),
+    ("12 PM", "12 PM"),
+    ("1 PM", "1 PM"),
+    ("2 PM", "2 PM"),
+    ("3 PM", "3 PM"),
+    ("4 PM", "4 PM"),
+    ("5 PM", "5 PM"),
+    ("6 PM", "6 PM"),
+    ("7 PM", "7 PM"),
+)
 
 class BusinessHour(models.Model):
     WEEKDAY_CHOICES = [
@@ -25,3 +39,18 @@ class BusinessHour(models.Model):
 
     class Meta:
         verbose_name_plural = _('Business hours')
+
+class Appointments(models.Model):
+    #user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    service = models.CharField(max_length=50,  default="Hair Cut")
+    barber=models.ForeignKey(Barbers, on_delete=models.CASCADE,null=True)
+    day = models.DateField(default=datetime.now)
+    time = models.CharField(max_length=10, choices=TIME_CHOICES, default="3 PM")
+    time_ordered = models.DateTimeField(default=datetime.now, blank=True)
+
+    class Meta:
+        verbose_name="Appointment"
+        verbose_name_plural='Appointmets'
+
+    def __str__(self):
+        return f"{self.service} | day: {self.day} | time: {self.time}"
